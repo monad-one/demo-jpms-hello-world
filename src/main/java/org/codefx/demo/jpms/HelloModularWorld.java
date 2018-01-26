b9fa707c0044bb0d0ecb70de29f55b1f694f36db
Nicolai Parlog <nipa@codefx.org>
2018-01-26 15:24:43 +0100

    Print module version at run time

--- src/main/java/org/codefx/demo/jpms/HelloModularWorld.java
+++ src/main/java/org/codefx/demo/jpms/HelloModularWorld.java
@@ -1,9 +1,15 @@
 package org.codefx.demo.jpms;
 
+import java.lang.module.ModuleDescriptor;
+
 public class HelloModularWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, modular World!");
+
+		Module module = HelloModularWorld.class.getModule();
+		ModuleDescriptor descriptor = module.getDescriptor();
+		System.out.println(descriptor.rawVersion());
 	}
 
 }

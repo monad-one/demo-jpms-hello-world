1fd7a78afb6de4e9016d41a7cf6bb15625cb500e
Nicolai Parlog <nipa@codefx.org>
2018-01-26 23:51:11 +0100

    Set module version at compile and package time

--- build.sh
+++ build.sh
@@ -10,10 +10,12 @@ mkdir -p target/classes
 echo " > compiling"
 javac9 \
 	-d target/classes \
+	--module-version 0.1 \
 	$(find src -name '*.java')
 
 echo " > packaging"
 jar9 --create \
 	--file target/hello-world.jar \
+	--module-version 0.2 \
 	--main-class org.codefx.demo.jpms.HelloModularWorld \
 	-C target/classes .
